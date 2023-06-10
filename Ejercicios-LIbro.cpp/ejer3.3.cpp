f6c8e84d7df1a394bd1092786fe8239499bf8a5c
nohelia45 <135685673+nohelia45@users.noreply.github.com>
2023-06-09 22:41:32 -0500

    REALIZADO

--- Ejercicios-LIbro.cpp/ejer3.3.cpp
+++ Ejercicios-LIbro.cpp/ejer3.3.cpp
@@ -1,5 +1,6 @@
 #include <cstdlib>
 #include <iostream>
+#include <cmath>
 using namespace std;
 int main(int argc, char *argv[])
 {
@@ -7,13 +8,17 @@ float a, b, c, d, e, f, denominador, x, y;
 cout << " Introduzca el valor de a de b y de c ";
 cin >> a >> b >> c;cout << " Introduzca el valor de d de e y de f " ;
 cin >> d >> e >> f;
-denominador = a * e * b * d;
+denominador = a * e * b * d; // Modificado calculo del denominador
 if (denominador == 0)
 cout << " no solucion\n";
 else
 {
 x = (c * e - b * f) / denominador;
-y = (a * f - c * d) / denominador;
+  if (x > 2){ // Modificado en el calculo del Y
+y = pow((a * f - c * d),2);
+} else { 
+    y = (a * f - c * d) - 2;
+  }
 cout << " la solucion del sistema es\n";
 cout << " x = " << x << " y = " << y << endl;
 }
