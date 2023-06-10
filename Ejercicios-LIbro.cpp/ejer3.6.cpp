c1384ff49e7e322c3c716c934ed7f6041375213f
nohelia45 <135685673+nohelia45@users.noreply.github.com>
2023-06-09 22:56:01 -0500

    REALIZADO

--- Ejercicios-LIbro.cpp/ejer3.6.cpp
+++ Ejercicios-LIbro.cpp/ejer3.6.cpp
@@ -7,16 +7,18 @@ int main(){
      double feet;
      cout << "introduzca pies:\n ";
       cin >> feet;
-      double patios = feet / 3;
+      double Yarda = feet / 3;
       double pulgadas = feet * 12;
       double cm = pulgadas * 2.54;
       double metros = cm / 100;
+      double Pies_Pulgadas = pulgadas / 12; // AGREGADO
 
       cout <<"equivalentes:"<<endl;
-      cout <<"patios:"<<feet<<endl;
-      cout <<"pies:"<<feet<<endl;
-      cout <<"pulgadas:"<<pulgadas<<endl;
-      cout <<"centimetros:"<<cm<<endl;
+      cout <<"Yardas: "<<Yarda<<endl;
+      cout <<"pies: "<<feet<<endl;
+      cout <<"pulgadas: "<<pulgadas<<endl;
+      cout <<"centimetros: "<<cm<<endl;
+      cout <<"Pulgadas A pies: " << Pies_Pulgadas << endl; // conversion de PULGADAS A PIES
 
       return 0;
 } // TRANSFORMAR PULGADAS A PIES
